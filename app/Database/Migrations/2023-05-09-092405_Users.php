f83f3fb7477ca387361aaf00dffb9fc3678f1d32
Rangqyunn <rdwitanara@gmail.comm>
2023-05-09 16:51:45 +0700

    COMIT USERS

---
+++ app/Database/Migrations/2023-05-09-092405_Users.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user'          => [
+                'type'           => 'INT',
+                'constraint'     => 3,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_pegawai'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '3',
+            ],
+            'username'       => [
+                'type'              => 'VARCHAR',
+                'constraint'        => "25",
+            ],
+            'password' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '100',
+            ],
+            'last_login' => [
+                'type'           => 'DATE'
+            ]
+        ]);
+        $this->forge->addPrimaryKey('id_user');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        //
+    }
+}
